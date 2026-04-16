d5032d47c5d2be0364c7c8cd1ef0ef981fb5a5a6
Rhegysa Juniartha <rhegysajuniartha@gmail.com>
2026-04-16 15:32:37 +0700

    4A1 Backend Done!

--- backend/src/app.js
+++ backend/src/app.js
@@ -31,6 +31,7 @@ const route1a5 = require('./routes/kepegawaian/1a5_tendik');
 
 //import Routes Sarpras
 const route3a1 = require('./routes/sarpras/3a1_sarana_prasarana');
+const route4a1 = require('./routes/sarpras/4a1_sarana_prasarana');
 
 // 2. Middleware Global
 app.use(cors()); 
@@ -64,6 +65,7 @@ app.use('/api/kepegawaian/1a5-tendik', route1a5);
 
 // Routes Sarpras
 app.use('/api/sarpras/3a1-sarana-prasarana', route3a1);
+app.use('/api/sarpras/4a1-sarana-prasarana-pkm', route4a1);
 
 // 4. Root Endpoint (Checking Status)
 app.get('/', (req, res) => {
